a559d2bb3037abfbab55ee1c5f003713491920dc
MartotoBG <martingav123@gmail.com>
2023-01-27 00:12:53 +0200

    added solutions

--- Programming-Advanced-JS/advanced-exercises 4/07. Hell's Kitchen/app.js
+++ Programming-Advanced-JS/advanced-exercises 4/07. Hell's Kitchen/app.js
@@ -2,7 +2,58 @@ function solve() {
    document.querySelector('#btnSend').addEventListener('click', onClick);
 
    function onClick () {
-      //   TODO:
+      let input = JSON.parse(document.querySelector('#inputs textarea').value);
+      let avgSalary = 0;
+      let totalSalary = 0;
+      let currAvgSalary = 0;
+      let bestName = '';
+      let output = {};
+      for (let line of input){
+         let restaurantInfo = line.split(' - ');
+         let restaurantName = restaurantInfo.shift();
+         let workersData = restaurantInfo[0].split(', ');
+         for (let worker of workersData){
+            let [name, salary] = worker.split(' ');
+            if(!output.hasOwnProperty(restaurantName)){
+               output[restaurantName] = {};
+            }
+            if(output.hasOwnProperty(restaurantName)) {
+               output[restaurantName][name] = Number(salary);
+            }
+         }
+
+      }
+      let entries = Object.entries(output)
+      for (let entry of entries){
+         let key = entry[0];
+         let values = Object.values(entry[1]);
+         for (let salary of values){
+            totalSalary += salary;
+         }
+         avgSalary = totalSalary / values.length;
+
+         if (avgSalary > currAvgSalary){
+
+            currAvgSalary = avgSalary;
+            bestName = key;
+            totalSalary = 0;
+         }
+      }
+
+      let result = Object.entries(output[bestName]).sort((a,b)=> b[1] - a[1]);
+      let print = '';
+
+      result.forEach(w => print += `Name: ${w[0]} With Salary: ${w[1]} `);
+
+
+      document.querySelector('#bestRestaurant p').textContent = `Name: ${bestName} Average Salary: ${currAvgSalary.toFixed(2)} Best Salary: ${(result[0][1]).toFixed(2)}`;
+
+
+      document.querySelector('#workers p').textContent = print;
       
    }
+
 }
+
+
+//["PizzaHut - Peter 500, George 300, Mark 800", "TheLake - Bob 1300, Joe 780, Jane 660"]
--- Programming-Advanced-JS/advanced-exercises 4/08. Generate Report/report.js
+++ Programming-Advanced-JS/advanced-exercises 4/08. Generate Report/report.js
@@ -1,3 +1,24 @@
 function generateReport() {
-    //TODO
+	const rows = document.querySelectorAll('tbody tr');
+    const columns = document.querySelectorAll('thead tr th');
+    const result = [];
+    
+    for (let i = 0 ; i<rows.length; i++){
+        const currentObject = {};
+        const data = rows[i].children; 
+        for (let j = 0; j<columns.length; j++){
+            
+           const isChecked = columns[j].children[0].checked === true;
+           if (isChecked){
+            const value = data[j].textContent;
+            const key = columns[j].textContent.toLowerCase().trim();
+            console.log(value)
+            currentObject[key] = value;
+           }
+        }
+        result.push(currentObject);
+    }
+    const json = JSON.stringify(result);
+    let output = document.getElementById('output')
+    output.textContent = json;
 }
