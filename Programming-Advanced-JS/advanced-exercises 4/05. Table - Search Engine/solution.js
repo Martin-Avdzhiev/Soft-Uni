9314bcf9c832f728a1b37760dad3fe6dbfa699d7
MartotoBG <martingav123@gmail.com>
2023-01-26 02:24:12 +0200

    added solutions

--- Programming-Advanced-JS/advanced-exercises 4/05. Table - Search Engine/solution.js
+++ Programming-Advanced-JS/advanced-exercises 4/05. Table - Search Engine/solution.js
@@ -2,7 +2,20 @@ function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
    function onClick() {
-      
+      const input = document.getElementById('searchField');
+      const elements = document.querySelectorAll('tbody tr');
+      const matches = [];
 
+      for (const el of elements) {
+         el.classList.remove('select');
+         if (input.value) {
+            if (el.textContent.toLowerCase().includes(input.value.toLowerCase())) {
+               matches.push(el.textContent);
+               el.classList = 'select';
+            }
+         }
+      }
+      input.value = '';
+      console.log(matches)
    }
 }
