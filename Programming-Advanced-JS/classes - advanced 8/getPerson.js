a18c992c3123ef2fc4eaa065107f967113f9cec0
MartotoBG <martingav123@gmail.com>
2023-02-08 01:58:47 +0200

    added solutions

---
+++ Programming-Advanced-JS/classes - advanced 8/getPerson.js
@@ -0,0 +1,20 @@
+function solve(){
+class Person {
+    constructor(firstName, lastName, age, email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.email = email;
+    }
+    toString() {
+        return `${this.firstName} ${this.lastName} (age: ${this.age}, email: ${this.email})`;
+    }
+}
+
+const p1 = new Person ('Anna','Simpson',22,'anna@yahoo.com');
+const p2 = new Person ('SoftUni');
+const p3 = new Person ('Stephan','Johnson',25);
+const p4 = new Person ('Gabriel','Peterson',24,'g.p@gmail.com');
+const result = [p1,p2,p3,p4];
+return result;
+}
