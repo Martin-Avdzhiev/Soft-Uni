70169d0553f84bbf9adece0f9d96511eab1b0cc1
MartotoBG <martingav123@gmail.com>
2023-02-24 14:41:26 +0200

    added solutions

--- Programming-Advanced-Applications-JS/Asynchronous programming - exercise/01.Bus-Stop/app.js
+++ Programming-Advanced-Applications-JS/Asynchronous programming - exercise/01.Bus-Stop/app.js
@@ -1,3 +1,20 @@
-function getInfo() {
-    console.log("TODO...");
+async function getInfo() {
+    const input = document.getElementById('stopId').value;
+    const url = `http://localhost:3030/jsonstore/bus/businfo/${input}`;
+    const stopName = document.getElementById('stopName');
+    const ul = document.getElementById('buses');
+    try {
+        ul.replaceChildren();
+        const response = await fetch(url);
+        const data = await response.json();  
+        stopName.innerText = data.name;
+        const entries = Object.entries(data.buses);
+        for (const entry of entries){
+            const li = document.createElement('li');
+            li.innerText = `Bus ${entry[0]} arrives in ${entry[1]} minutes`;
+            ul.appendChild(li)
+        }
+    } catch {
+        stopName.innerText = 'Error';
+    }
 }
