6082dfffbdb3a8ca72c0cbaf9ea1fe7896197e84
MartotoBG <martingav123@gmail.com>
2023-03-08 11:42:04 +0200

    added solutions

--- Programming-Advanced-Applications-JS/Remote Data and Authentication - exercise/05.Fisher-Game/src/login.js
+++ Programming-Advanced-Applications-JS/Remote Data and Authentication - exercise/05.Fisher-Game/src/login.js
@@ -1,1 +1,35 @@
-console.log('TODO:// Implement Login functionality');
+const loginForm = document.querySelector('form');
+loginForm.addEventListener('submit', loginUser);
+document.getElementById('user').style.display = 'none';
+const loginUrl = 'http://localhost:3030/users/login';
+async function loginUser(e){
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const email = formData.get('email');
+    const password = formData.get('password');
+
+    try {
+        const response = await fetch(loginUrl,{
+            method: 'POST',
+            headers: {'Content-type': 'application-json'},
+            body: JSON.stringify({
+                email,
+                password
+            })
+        })
+        if(!response.ok){
+            const error = await response.json();
+            throw new Error(error.message);
+        }
+        const data = await response.json();
+        const userData = {
+            email: data.email,
+            id: data._id,
+            token: data.accessToken
+        }
+        localStorage.setItem('userData', JSON.stringify(userData));
+        window.location = './index.html'
+    } catch (error) {
+        alert(error.message);
+    }
+}
--- Programming-Advanced-Applications-JS/Remote Data and Authentication - exercise/05.Fisher-Game/src/register.js
+++ Programming-Advanced-Applications-JS/Remote Data and Authentication - exercise/05.Fisher-Game/src/register.js
@@ -1,1 +1,43 @@
-console.log('TODO:// Implement Register functionality');
+const form = document.querySelector('form');
+form.addEventListener('submit', registerUser);
+const registerUrl = 'http://localhost:3030/users/register';
+document.getElementById('user').style.display = 'none';
+async function registerUser(e){
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const { email, password, rePass } = Object.fromEntries(formData);
+    const array = [... formData.values()];
+    try {
+        if(array.some(x => x=== '')){
+            throw new Error('All inputs must be filled');
+        }
+        if(array[1] !== array[2]){
+            throw new Error('Passwords don\'t match');
+        }
+        
+        const response = await fetch(registerUrl,{
+            method: 'POST',
+            headers: {'Content-type': 'application/json'},
+            body: JSON.stringify({
+                email,
+                password,
+                rePass
+            })
+        });
+        if(!response.ok){
+            const error = await response.json();
+            throw new Error(error.message);
+        }
+        const data = await response.json();
+        console.log(data)
+        const user = {
+            email: data.email,
+            id: data._id,
+            token: data.accessToken
+        }
+        localStorage.setItem('userData', JSON.stringify(user));
+        window.location = './index.html';
+    } catch (error) {
+        alert(error.message)
+    }
+}
