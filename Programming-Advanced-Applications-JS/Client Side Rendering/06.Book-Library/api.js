c8ef6d1cb3e2c7e75685fc245715e9146646c62f
MartotoBG <martingav123@gmail.com>
2023-03-15 22:59:41 +0200

    added solutions

--- Programming-Advanced-Applications-JS/Client Side Rendering/06.Book-Library/api.js
+++
@@ -1,7 +0,0 @@
-const url = 'http://localhost:3030/jsonstore/collections/books';
-export async function get(){
-    const response = await fetch(url);
-    const data = await response.json();
-    const array = {};
-    return data;
-}
--- Programming-Advanced-Applications-JS/Client Side Rendering/06.Book-Library/app.js
+++ Programming-Advanced-Applications-JS/Client Side Rendering/06.Book-Library/app.js
@@ -1,2 +1,74 @@
-import { get } from "./api.js";
-import { renderring } from "./rowTemplate.js";
+import { render } from './node_modules/lit-html/lit-html.js';
+import { getAllBooks, createBook, updateBook, deleteBook } from './src/api.js';
+import { mainTemplate } from './src/templates/mainTemplate.js';
+import { tableRowsTemplate } from './src/templates/tableRowsTemplate.js';
+import { editButtonHandler } from './src/actions.js';
+const documentBody = document.querySelector('body');
+render(mainTemplate(), documentBody);
+
+documentBody.querySelector('#loadBooks').addEventListener('click', async () => {
+    const booksData = await getAllBooks();
+    const section = documentBody.querySelector('table tbody');
+    const books = [];
+    for (const id in booksData) {
+        books.push({
+            author: booksData[id].author,
+            title: booksData[id].title,
+            _id: id
+        })
+    }
+    const context = {
+        books,
+        deleteFunction,
+        editButtonHandler
+    }
+    render(tableRowsTemplate(context), section)
+
+})
+    const addFormElement = documentBody.querySelector('#add-form');
+    addFormElement.addEventListener('submit', async (e)=> {
+        e.preventDefault();
+        const formData = new FormData(addFormElement);
+        const author = formData.get('author');
+        const title = formData.get('title');
+        if(!title || !author){
+            return alert('Please fill all fields');
+        }
+        const book = {
+            author,
+            title
+        }
+        await createBook(book).then(data => {
+            addFormElement.reset();
+            documentBody.querySelector('#loadBooks').click();
+        })
+    })
+async function deleteFunction(id){
+    await deleteBook(id);
+    documentBody.querySelector('#loadBooks').click();
+}
+
+const editFormElement = documentBody.querySelector('#edit-form');
+
+editFormElement.addEventListener('submit',async (e)=>{
+    e.preventDefault();
+    const formData = new FormData(editFormElement);
+    const id = formData.get('id');
+    const author = formData.get('author');
+    const title = formData.get('title');
+    if(!title || !author){
+        return alert('Please fill all fields');
+    };
+    const book = {
+        author,
+        title
+    }
+
+    await updateBook(id,book).then(() =>{
+        documentBody.querySelector('#loadBooks').click();
+        editFormElement.style.display = 'none';
+        addFormElement.style.display = 'block';
+        editFormElement.reset();
+        documentBody.querySelector('#loadBooks').click();
+    })
+})
