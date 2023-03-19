1c1923a1ddc4a2462d5c3fb390dc8476a052b6d4
MartotoBG <martingav123@gmail.com>
2023-03-19 03:40:05 +0200

    fix some bugs

--- Programming-Advanced-Applications-JS/Routing - exercise/01.Furniture/views/deleteView.js
+++ Programming-Advanced-Applications-JS/Routing - exercise/01.Furniture/views/deleteView.js
@@ -2,7 +2,7 @@ import { del } from '../api.js';
 import page from '../node_modules/page/page.mjs';
 
 export function onClick(e) { 
-    console.log('hi')
+  
     const confirmDeletion = confirm('Are you sure?');
     if(confirmDeletion){
         del(`/data/catalog/${e.target.id}`);
