39668950fa004bfef2b34e55e800eecca0d3a9e5
MartotoBG <martingav123@gmail.com>
2023-04-28 01:48:29 +0300

    fix

--- JS-Web/Workshop ExpressJS and Templating/src/index.js
+++ JS-Web/Workshop ExpressJS and Templating/src/index.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const handlebars = require('express-handlebars');
 
 const config = require('./config');
 
 const app = express();
 
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs',
+}));
+
+app.set('view engine', 'hbs');
+app.set('views','./src/views');
+
 app.get('/',(req,res)=> {
-        res.send('Home Page');
+        res.render('home')
 });
 
 app.listen(config.PORT, () => console.log(`Server is running on port ${config.PORT}...`));
