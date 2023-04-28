ba976fdf5dc3ef9f5b5246e58d878e9b359df363
MartotoBG <martingav123@gmail.com>
2023-04-29 02:17:21 +0300

    add database

--- JS-Web/Workshop ExpressJS and Templating/cubicle/src/routes.js
+++ JS-Web/Workshop ExpressJS and Templating/cubicle/src/routes.js
@@ -1,6 +1,6 @@
 const cubeController = require('./controllers/cubeController');
 const router = require('express').Router();
-
+const database = require('./database/cubes.json');
 
 router.get('/', (req, res) => {
     res.render('index')
@@ -10,6 +10,13 @@ router.get('/about', (req, res) => {
     res.render('about');
 });
 
+router.get('/details/:id', (req, res) => {
+    const paramsObj = req.params;
+    const id = paramsObj.id;
+    const cube = database.id;
+    res.send(JSON.stringify(database))
+    //res.render('details');
+})
 router.get('/create', cubeController.getCreateCube);
 
 module.exports = router;
