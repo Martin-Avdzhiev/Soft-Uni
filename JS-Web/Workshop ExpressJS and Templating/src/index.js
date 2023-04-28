d5afbf1561b094e825dd4e4a9d527b325df7d098
MartotoBG <martingav123@gmail.com>
2023-04-28 13:39:26 +0300

    add express handlebars

--- JS-Web/Workshop ExpressJS and Templating/src/index.js
+++ JS-Web/Workshop ExpressJS and Templating/src/index.js
@@ -1,16 +1,12 @@
 const express = require('express');
-const handlebars = require('express-handlebars');
 
 const config = require('./config');
+const setupViewEngine = require('./config/viewEngine');
 
 const app = express();
+setupViewEngine(app);
 
-app.engine('hbs', handlebars.engine({
-    extname: 'hbs',
-}));
 
-app.set('view engine', 'hbs');
-app.set('views','./src/views');
 
 app.get('/',(req,res)=> {
         res.render('home')
