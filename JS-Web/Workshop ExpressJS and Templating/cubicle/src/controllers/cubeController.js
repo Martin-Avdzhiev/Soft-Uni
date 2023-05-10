9b6e4c68f12dbd2fb224d7e578d631872a63ad94
MartotoBG <martingav123@gmail.com>
2023-05-10 17:01:27 +0300

    add unique ids

--- JS-Web/Workshop ExpressJS and Templating/cubicle/src/controllers/cubeController.js
+++ JS-Web/Workshop ExpressJS and Templating/cubicle/src/controllers/cubeController.js
@@ -1,5 +1,23 @@
-
-
+const crypto = require('crypto');
+const database = require('../database/cubes.json');
+const fs = require('fs');
+const path = require('path');
 exports.getCreateCube = (req, res) => {
     res.render('create');
+}
+
+exports.postCreateCube= (req, res) => {
+    const id = crypto.randomBytes(16).toString('hex');
+    const cube = {
+        id: id,
+        name: req.body.name,
+        description: req.body.description,
+        imageUrl: req.body.imageUrl,
+        difficultyLevel: Number(req.body.difficultyLevel)
+    }
+    database.cubes.push(cube);
+    const json = JSON.stringify(database, null, 2);
+    console.log(json)
+    fs.writeFileSync(path.resolve(__dirname, '../database/cubes.json'), json);
+    res.redirect('/')
 }
--- JS-Web/Workshop ExpressJS and Templating/cubicle/src/routes.js
+++ JS-Web/Workshop ExpressJS and Templating/cubicle/src/routes.js
@@ -3,7 +3,8 @@ const router = require('express').Router();
 const database = require('./database/cubes.json');
 
 router.get('/', (req, res) => {
-    res.render('index')
+    const cubes = database;
+    res.render('index', cubes);
 });
 
 router.get('/about', (req, res) => {
@@ -13,10 +14,13 @@ router.get('/about', (req, res) => {
 router.get('/details/:id', (req, res) => {
     const paramsObj = req.params;
     const id = paramsObj.id;
-    const cube = database.id;
-    res.send(JSON.stringify(database))
-    //res.render('details');
+    const cube = database.cubes.filter(x => x["id"] == id)[0];
+    res.render('details', cube);
 })
 router.get('/create', cubeController.getCreateCube);
-
+router.post('/create', cubeController.postCreateCube);
 module.exports = router;
+router.get('*', (req, res) => {
+    res.render('404');
+})
+
