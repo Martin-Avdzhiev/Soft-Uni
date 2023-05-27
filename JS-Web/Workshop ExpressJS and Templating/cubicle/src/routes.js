fe1c8a428c9f4de90825c3f8fc887dfd088644c6
MartotoBG <107769673+MartotoBG@users.noreply.github.com>
2023-05-27 20:15:52 +0300

    add login and register page

--- JS-Web/Workshop ExpressJS and Templating/cubicle/src/routes.js
+++ JS-Web/Workshop ExpressJS and Templating/cubicle/src/routes.js
@@ -2,12 +2,13 @@ const router = require('express').Router();
 const cubeController = require('./controllers/cubeController');
 const homeController = require('./controllers/homeController');
 const accessoryController = require('./controllers/accessoryController');
+const authController = require('./controllers/authController');
 const Cube = require('./models/Cube');
 
 router.get('/', homeController.getHomePage);
 
 router.get('/about', (req, res) => res.render('about'));
-
+router.use('/auth', authController);
 router.get('/cubes/:id/details', cubeController.getDetails);
 router.get('/cubes/create', cubeController.getCreateCube);
 router.post('/cubes/create', cubeController.postCreateCube);
