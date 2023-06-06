c1ec580050eb7dfdd9b040d50c1d663c40a76043
MartotoBG <martingav123@gmail.com>
2023-06-06 10:31:00 +0300

    move router homecontroller above authcontroller

--- JS-Web/skeleton/routes.js
+++ JS-Web/skeleton/routes.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 const homeController = require('./controllers/homeController');
 const authController = require('./controllers/authController');
 
-router.use(homeController);
 router.use(authController);
+router.use(homeController);
 
 module.exports = router;
