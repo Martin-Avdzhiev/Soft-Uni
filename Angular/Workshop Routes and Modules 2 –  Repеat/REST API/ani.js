c2157f76613f5919083eedc5887e57ae974daf78
MartotoBG <martingav123@gmail.com>
2023-07-10 10:19:25 +0300

    delete the repeated workshop

--- Angular/Workshop Routes and Modules 2 –  Repеat/REST API/ani.js
+++
@@ -1,9 +0,0 @@
-function uchebnici(input) {
-    let pages = Number(input[0])
-    let pagesPerHour = Number(input[1])
-    let days = Number(input[2])
-    let neededHours =  ((pages / pagesPerHour) / days)
-    console.log(neededHours)
-
-}
-uchebnici(["212", "20", "0"])
