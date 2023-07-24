9bdf82629ee2bdf516ce4d34fa5e79f5a37db406
MartotoBG <martingav123@gmail.com>
2023-07-24 21:33:07 +0300

    update the navigation bar

--- Angular/Project/Crypto/src/app/core/header/header.component.ts
+++ Angular/Project/Crypto/src/app/core/header/header.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, OnChanges, SimpleChanges, DoCheck } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
-
+export class HeaderComponent implements OnInit, DoCheck{
+    username: string | undefined;
+    constructor(private cookieService: CookieService){}
+   ngOnInit(): void {
+     this.username = this.cookieService.get('username');
+   }
+   ngDoCheck(): void {
+    this.username = this.cookieService.get('username');
+   }
+   logout():void{
+    this.cookieService.deleteAll();
+   }
 }
