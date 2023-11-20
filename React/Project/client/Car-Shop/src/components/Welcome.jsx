3ee7f8e3d4d661a77017e3dfa19863c8ddf83f5b
MartotoBG <martingav123@gmail.com>
2023-11-20 19:43:06 +0200

    add css transitions to welcome page

--- React/Project/client/Car-Shop/src/components/Welcome.jsx
+++ React/Project/client/Car-Shop/src/components/Welcome.jsx
@@ -1,27 +1,48 @@
-import './styles/Welcome.css';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { CSSTransition } from 'react-transition-group';
+import './styles/Welcome.css';
+import './styles/transformTransition.css';
 import welcomeCar from '../assets/welcome-car.jpg';
 import welcomeMotorbike from '../assets/welcome-motorbike.jpg';
 export default function Welcome() {
+    const [isEnterCar, setIsEnterCar] = useState(false);
+    const [isEnterMotorbike, setIsEnterMotorbike] = useState(false);
     return (
         <>
-        <div className='welcome'>
-            <div className='welcome-message'>
-            <h1>Welcome to our Car Shop</h1>
-            <h3>psss, we offer motorbikes too...</h3>
+            <div className='welcome'>
+                <div className='welcome-message'>
+                    <h1>Welcome to our Car Shop</h1>
+                    <h3>psss, we offer motorbikes too...</h3>
+                </div>
+                <p>Explore our wide range of cars, motorbikes.</p>
+                <p>Don't have account?</p>
+                <Link to="/signup">SIGN UP</Link>
             </div>
-            <p>Explore our wide range of cars, motorbikes.</p>
-            <p>Don't have account?</p>
-            <Link to="/signup">SIGN UP</Link>
-        </div>
-        <div className='welcome-images'>
-            <div className='welcome-car'>
-                <img src={welcomeCar} alt="Car" />
+            <div className='welcome-images'>
+                <div className='welcome-car'>
+                    <Link to={'cars'} onMouseEnter={() => setIsEnterCar((v) => true)} onMouseLeave={() => setIsEnterCar((v) => false)}>
+                        <CSSTransition
+                            in={isEnterCar}
+                            timeout={300}
+                            classNames="transition"
+                        >
+                            <img src={welcomeCar} alt="Cars" />
+                        </CSSTransition>
+                    </Link>
+                </div>
+                <div className='welcome-motorbike'>
+                    <Link to={'motorbikes'} onMouseEnter={() => setIsEnterMotorbike((v) => true)} onMouseLeave={() => setIsEnterMotorbike((v) => false)}>
+                        <CSSTransition
+                            in={isEnterMotorbike}
+                            timeout={300}
+                            classNames="transition"
+                        >
+                            <img src={welcomeMotorbike} alt="Motorbikes" />
+                        </CSSTransition>
+                    </Link>
+                </div>
             </div>
-            <div className='welcome-motorbike'>
-                <img src={welcomeMotorbike} alt="Motorbike" />
-            </div>
-        </div>
         </>
     )
 }
