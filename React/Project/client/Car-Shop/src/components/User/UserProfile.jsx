2888394418bd9242d5a9507dd85077675683a028
MartotoBG <martingav123@gmail.com>
2023-11-23 16:46:00 +0200

    add delete confirm modal in user page

--- React/Project/client/Car-Shop/src/components/User/UserProfile.jsx
+++ React/Project/client/Car-Shop/src/components/User/UserProfile.jsx
@@ -4,76 +4,93 @@ import AuthContext from '../../contexts/authContext';
 import { getProfileInfo } from '../../services/authServices';
 import { Link } from 'react-router-dom';
 export default function UserProfile() {
-    const [userInfo, setUserInfo] = useState({})
+    const [userInfo, setUserInfo] = useState({});
+    const [isModalOpen, setModalOpen] = useState(false);
     const {
         _id
     } = useContext(AuthContext);
+
+    const openModal = () => {setModalOpen(true);}
+
+    const closeModal = () => {setModalOpen(false);}
+
+    const deleteVehicle = async () => {
+        console.log('delete')
+    }
 
     useEffect(() => {
         getProfileInfo(_id).then(result => setUserInfo(result));
     }, [])
     return (
-        <div className="user-container">
-            <div className="user-image">
-                <img src="https://cdn-icons-png.flaticon.com/512/149/149071.png" alt="User Image" />
-            </div>
-            <div className="user-info">
-                <div className="username">{userInfo.username}</div>
-                <div className="user-details">
-                    <p>{userInfo.email}</p>
+        <>
+            <div className="user-container">
+                <div className="user-image">
+                    <img src="https://cdn-icons-png.flaticon.com/512/149/149071.png" alt="User Image" />
                 </div>
-            </div>
-            <div className='create-buttons-container'>
-                <div className="create-buttons">
-                    <Link to="/create/car">
-                        <button className="create-car-button">Create New Car</button>
-                    </Link>
-                    <Link to="/create/motorbike">
-                        <button className="create-motorbike-button">Create New Motorbike</button>
-                    </Link>
+                <div className="user-info">
+                    <div className="username">{userInfo.username}</div>
+                    <div className="user-details">
+                        <p>{userInfo.email}</p>
+                    </div>
                 </div>
-            </div>
-            <div className="vehicles-wrapper">
-                <div className="vehicle-section">
-                    <p className='offers'>Your car offers:</p>
-                    {userInfo.ownCars?.map(car => (
-                        <div className="vehicle" key={car._id}>
-                            <div className="vehicle-image">
-                                <img src={car.imageUrl} alt={car.name} />
-                            </div>
-                            <div className="vehicle-info">
-                                <p>{car.name}</p>
-                            </div>
-                            <div className="vehicle-buttons">
-                                <button className="edit-button">Edit</button>
-                                <button className="delete-button">Delete</button>
-                            </div>
-                        </div>
-                    ))}
+                <div className='create-buttons-container'>
+                    <div className="create-buttons">
+                        <Link to="/create/car">
+                            <button className="create-car-button">Create New Car</button>
+                        </Link>
+                        <Link to="/create/motorbike">
+                            <button className="create-motorbike-button">Create New Motorbike</button>
+                        </Link>
+                    </div>
                 </div>
-                <div className="vehicle-section">
-                    <p className='offers'>Your motorbike offers:</p>
-                    {userInfo.ownMotorbikes?.map(motorbike => (
-                        <div className="vehicle">
-                            <div className="vehicle-image">
-                                <img
-                                    src={motorbike.imageUrl}
-                                    alt={motorbike.name}
-                                />
+                <div className="vehicles-wrapper">
+                    <div className="vehicle-section">
+                        <p className='offers'>Your car offers:</p>
+                        {userInfo.ownCars?.map(car => (
+                            <div className="vehicle" key={car._id}>
+                                <div className="vehicle-image">
+                                    <img src={car.imageUrl} alt={car.name} />
+                                </div>
+                                <div className="vehicle-info">
+                                    <p>{car.name}</p>
+                                </div>
+                                <div className="vehicle-buttons">
+                                    <button className="edit-button">Edit</button>
+                                    <button className="delete-button" onClick={openModal}>Delete</button>
+                                </div>
                             </div>
-                            <div className="vehicle-info">
-                                <p>{motorbike.name}</p>
+                        ))}
+                    </div>
+                    <div className="vehicle-section">
+                        <p className='offers'>Your motorbike offers:</p>
+                        {userInfo.ownMotorbikes?.map(motorbike => (
+                            <div className="vehicle" key={motorbike._id}>
+                                <div className="vehicle-image">
+                                    <img
+                                        src={motorbike.imageUrl}
+                                        alt={motorbike.name}
+                                    />
+                                </div>
+                                <div className="vehicle-info">
+                                    <p>{motorbike.name}</p>
+                                </div>
+                                <div className="vehicle-buttons">
+                                    <button className="edit-button">Edit</button>
+                                    <button className="delete-button" onClick={openModal}>Delete</button>
+                                </div>
                             </div>
-                            <div className="vehicle-buttons">
-                                <button className="edit-button">Edit</button>
-                                <button className="delete-button">Delete</button>
-                            </div>
-                        </div>
-                    ))}
+                        ))}
 
+                    </div>
                 </div>
             </div>
-        </div>
-
+            <div className={`modal ${isModalOpen ? ' open' : ''}`}>
+                <div className="modal-content">
+                    <p>Are you sure you want to delete it?</p>
+                    <button onClick={deleteVehicle}>Yes</button>
+                    <button onClick={closeModal}>No</button>
+                </div>
+            </div>
+        </>
     )
 }
