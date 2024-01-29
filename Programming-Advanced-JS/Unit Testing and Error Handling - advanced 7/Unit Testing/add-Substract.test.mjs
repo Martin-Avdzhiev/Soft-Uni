386aa17e651525b792acd341dd76576c1a8e0f41
MartotoBG <107769673+MartotoBG@users.noreply.github.com>
2024-01-29 22:53:00 +0200

    learn new changes about mocha and chai

---
+++ Programming-Advanced-JS/Unit Testing and Error Handling - advanced 7/Unit Testing/add-Substract.test.mjs
@@ -0,0 +1,53 @@
+import * as chai from 'chai'
+const expect = chai.expect;
+import createCalculator from './add-Substract.mjs';
+
+describe('Calculator', function (){
+    let value = null;
+   beforeEach(()=> {
+    value = createCalculator();
+   });
+   it ('return 0 initial value', ()=> {
+    expect(value.get()).to.equal(0);
+   });
+   it('return 10 when add 10', ()=>{
+    value.add(10);
+    expect(value.get()).to.equal(10);
+   });
+   it('return -10 when substract 10', ()=>{
+    value.subtract(10);
+    expect(value.get()).to.equal(-10);
+   });
+   it('return 0 when add 10 and substract 10', ()=>{
+    value.add(10);
+    value.subtract(10);
+    expect(value.get()).to.equal(0);
+   });
+   it('return 20 when add 10 twice', ()=>{
+    value.add(10);
+    value.add(10);
+    expect(value.get()).to.equal(20);
+   });
+   it('return -20 when subtract 10 twice', ()=>{
+    value.subtract(10);
+    value.subtract(10);
+    expect(value.get()).to.equal(-20);
+   });
+   it('return 50 when add 100, subtract 25, add 50, subtract 75', ()=>{
+    value.add(100);
+    value.subtract(25);
+    value.add(50);
+    value.subtract(75);
+    expect(value.get()).to.equal(50);
+   });
+   it('return 20 when add 10 twice but one 10 is in string', ()=>{
+    value.add(10);
+    value.add('10');
+    expect(value.get()).to.equal(20);
+   });
+   it('return -20 when subtract 10 twice but one 10 is in string', ()=>{
+    value.subtract(10);
+    value.subtract('10');
+    expect(value.get()).to.equal(-20);
+   });
+})
