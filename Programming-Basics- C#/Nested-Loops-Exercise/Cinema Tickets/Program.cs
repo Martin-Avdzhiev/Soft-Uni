4d3be7e010b49039653584e7660cf4f820b531d3
MartotoBG <martingav123@gmail.com>
2024-02-08 09:25:32 +0200

    completed more c# exercises

---
+++ Programming-Basics- C#/Nested-Loops-Exercise/Cinema Tickets/Program.cs
@@ -0,0 +1,41 @@
+﻿int kidTickets = 0;
+int studentTickets = 0;
+int standardTickets = 0;
+
+string movieName = Console.ReadLine();
+while (movieName != "Finish")
+{
+    int freeSeats = int.Parse(Console.ReadLine());
+    int tickets = 0;
+    string ticketType = Console.ReadLine();
+    while (ticketType != "End")
+    {
+        if (ticketType == "standard")
+        {
+            standardTickets++;
+        }
+        else if (ticketType == "student")
+        {
+            studentTickets++;
+
+        }
+        else
+        {
+            kidTickets++;
+        }
+        tickets++;
+        if (tickets == freeSeats)
+        {
+            break;
+        }
+        ticketType = Console.ReadLine();
+    }
+    
+    Console.WriteLine($"{movieName} - {tickets * 100.0 / freeSeats:f2}% full.");
+    movieName = Console.ReadLine();
+}
+double totalTickets = kidTickets + standardTickets + studentTickets;
+Console.WriteLine($"Total tickets: {totalTickets}");
+Console.WriteLine($"{studentTickets * 100 / totalTickets:F2}% student tickets.");
+Console.WriteLine($"{standardTickets * 100 / totalTickets:F2}% standard tickets.");
+Console.WriteLine($"{kidTickets * 100 / totalTickets:F2}% kids tickets.");
---
+++ Programming-Basics- C#/Nested-Loops-Exercise/Special Numbers/Program.cs
@@ -0,0 +1,30 @@
+﻿int number = int.Parse(Console.ReadLine());
+int start = 1111;
+int end = 9999;
+string result = "";
+for (int i = start; i <= end; i++)
+{
+    bool isSpecial = true;
+    string currentNumber = i.ToString();
+    for (int j = 0; j < 4; j++)
+    {
+        double numericValue = char.GetNumericValue(currentNumber[j]);
+        int numberOnCurrentIndex = (int)numericValue;
+        if (numberOnCurrentIndex != 0)
+        {
+            if (number % numberOnCurrentIndex != 0)
+            {
+                isSpecial = false;
+            }
+        }
+        else
+        {
+            isSpecial = false;
+        }
+    }
+    if (isSpecial)
+    {
+        result += currentNumber + " ";
+    }
+}
+Console.WriteLine(result);
